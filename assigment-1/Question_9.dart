937916b52226954d7945a3547815ccf60b20159f
Aamir Siddique <122437635+Aamirsiddique09@users.noreply.github.com>
2023-06-27 02:19:24 +0500

    Update Question_9.dart

--- assigment-1/Question_9.dart
+++ assigment-1/Question_9.dart
@@ -1,3 +1,4 @@
+/*---------------SMIT FLUTTER ----------------*/
 void main() {
   // Check if the number is even or odd, If number is even then check if this is divisible by 5
   //or not & if number is odd then check if this is divisible by 7 or not?
